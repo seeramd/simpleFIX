919738170975ba4d68cfc54052273e8838698aa2
Divendra Seeram <dseeram@proton.me>
2023-07-15 21:24:34 -0400

    Functionality to tail a local log file

--- fix.pl
+++ fix.pl
@@ -1,21 +1,21 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use Data::Dumper;
 use DBI;
+use File::Tail;
 
 my $dsn = "DBI:SQLite:dbname=data/fix_dictionary.db";
 my $conn = DBI->connect($dsn, "", "", {RaiseError => 1}) or die $DBI::errstr;
+my $log_path = "sample.log";
 my $DELIMITER='\|';
 
 print "Successfully connected\n";
 
 sub retrieve_tag_names {
     my @tags = @_;
-    my $tag;
     my @values;
 
-    foreach $tag (@tags) {
+    foreach my $tag (@tags) {
         my $query = $conn->prepare("SELECT name FROM fix_tags WHERE tag == $tag");
         $query->execute() or die $DBI::errstr;
         if (my @row = $query->fetchrow) {
@@ -29,15 +29,14 @@ sub retrieve_tag_names {
     return @values;
 }
 
-open(my $fh, "<", "sample.log") or die "Could not open input file: $!";
+my $file = File::Tail->new(name=>$log_path, maxinterval=>5, interval=>1, tail=>-1);
 my %tag_hash;
+while(defined(my $line = $file->read)) {
+    chomp $line;
+    my @fix_tags = split /$DELIMITER/, $line;
 
-while(<$fh>) {
-    chomp;
-    my @fix_tags = split /$DELIMITER/, $_;
-
-    foreach(@fix_tags) {
-        my ($tag_number, $value) = split /=/, $_;
+    foreach my $tag_value (@fix_tags) {
+        my ($tag_number, $value) = split /=/, $tag_value;
         my @tag_name = retrieve_tag_names($tag_number);
         $tag_hash{$tag_name[0]} = $value;
     }
